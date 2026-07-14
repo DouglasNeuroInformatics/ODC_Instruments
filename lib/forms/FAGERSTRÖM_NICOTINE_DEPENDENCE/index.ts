c2b8bcee9b3320a5b1d8d89e0f2e47702599f608
David Roper <davidroper321@gmail.com>
2026-07-14 13:27:41 -0400

    increment edition

--- lib/forms/FAGERSTRÖM_NICOTINE_DEPENDENCE/index.ts
+++ lib/forms/FAGERSTRÖM_NICOTINE_DEPENDENCE/index.ts
@@ -31,7 +31,7 @@ export default defineInstrument({
   language: ['en', 'fr'],
   internal: {
     name: 'FAGERSTRÖM_NICOTINE_DEPENDENCE',
-    edition: 1
+    edition: 2
   },
   tags: {
     en: ['smoking', 'addiction', 'nicotine'],
